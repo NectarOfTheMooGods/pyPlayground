d990ac96f94e6646452ed0870b9994bf4d0f689b
James <jgavigan@gmail.com>
2016-12-19 13:10:19 -0800

    new string format

--- weather.py
+++ weather.py
@@ -12,6 +12,10 @@ import datetime
 import debugger
 
 
+numDays = 10
+degreeSign = '\u00b0'
+percentSign = '\u0025'
+
 
 def RequestWeatherData(url):
     try:
@@ -35,32 +39,27 @@ def KelvinToFahrenheit(k):
 def main(argv):
     #debugger.attachDebugger()
 
-    numDays = 7
-    degreeSign = '\u00b0'
-    percentSign = '\u0025'
-
     site = 'http://api.openweathermap.org/data/2.5/'
     current = 'weather?'
     forecast = 'forecast/daily?'
     city = 'id=5384690&'
-    length = 'cnt=%s&' %(numDays)
+    length = 'cnt={}&'.format(numDays)
     appID = 'APPID=63a3b02da3da17793691ad1e43c17fb4'
 
     url = site + current + city + appID
     weatherData = RequestWeatherData(url)
 
-    print("Current weather for %s:" %(weatherData['name']))
+    print("Current weather for {}:".format(weatherData['name']))
     curTemp = KelvinToFahrenheit(weatherData['main']['temp'])
     humidity = weatherData['main']['humidity']
-    #desc = weatherData['weather'][0]['main'] + " - " + weatherData['weather'][0]['description']
     desc = weatherData['weather'][0]['description']
-    print("%.1f%s %s%s humidity  %s" %(curTemp, degreeSign, humidity, percentSign, desc))
+    print("{:.1f}{} - {}{} humidity - {}".format(curTemp, degreeSign, humidity, percentSign, desc))
     
 
     url = site + forecast + city + length + appID
     weatherData = RequestWeatherData(url)
 
-    print("%s Day Forecast:" %(numDays))
+    print("{} Day Forecast:".format(numDays))
 
     # build the date strings, with "today" and "tomorrow" for the first 2 slots
     dateStr = ['Today','Tomorrow']
@@ -71,12 +70,11 @@ def main(argv):
 
     #zip allows to iterate though 2 lists in tandem
     for date, w in zip(dateStr, weatherData['list']):
-        #desc = w['weather'][0]['main'] + " - " + w['weather'][0]['description']
         desc = w['weather'][0]['description']
         hi = KelvinToFahrenheit(w['temp']['max'])
         low = KelvinToFahrenheit(w['temp']['min'])
         date = date.ljust(10)
-        print("%s: Hi: %.1f%s Low: %.1f%s   %s" %(date, hi, degreeSign, low, degreeSign, desc))
+        print("{}: Hi: {:.1f}{} Low: {:.1f}{} - {}".format(date, hi, degreeSign, low, degreeSign, desc))
 
 
 if __name__ == "__main__":
@@ -85,3 +83,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("main KeyboardInterrupt")
         pass
+    
